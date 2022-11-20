9ad33491e84a7ca6773bde05d5e18db5abe6b0c6
MrSarno <email@sarno.dev>
2022-11-20 17:39:19 +0000

    DEV: add d1_ownership_rules

---
+++ d1_ownership_rules/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("OWNERSHIP RULES");
+    println!("\n1. Each value in Rust has a variable that's called it's owner.");
+    println!("2. There can only be one owner for each variable at any one time.");
+    println!("3. When the owner goes out of scope the value is dropped.");
+}
